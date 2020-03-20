9bdd761d6b78e92adb59a77358a5ec04095444b2
Your Name <you@example.com>
2020-03-20 13:18:46 -0600

    css connects to the html elements 'box-class' and javascript classes are created that mirror the types of boxes requested by Priscilla

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 class Box
 {
-    constructor(height, width)
+    constructor()
     {
-        this.height = height;
-        this.width = width;
+        this.typeMessage = "I'm a Box";
     }
 }
 
-function Templates(height, width)
+class Templates extends Box
 {
-    Box.call(this, height, width);
+    constructor()
+    {
+        super();
+        this.typeMessage = "I'm a Template";
+    }
 }
-function Activities(height, width)
+class Activities extends Box
 {
-    Box.call(this, height, width);
+    constructor()
+    {
+        super();
+        this.typeMessage = "I'm an Activity";
+    }
 }
+
+
+
+var boxes = document.getElementsByClassName("box-class")
+console.log(boxes);
+
+var box_obj = new Box();
+console.log(boxes[0]);
+boxes[0].innerHTML = box_obj.typeMessage;
+
+var template_obj = new Templates();
+console.log(boxes[0]);
+boxes[1].innerHTML = template_obj.typeMessage;
+
+var activity_obj = new Activities();
+console.log(boxes[0]);
+boxes[2].innerHTML = activity_obj.typeMessage;
