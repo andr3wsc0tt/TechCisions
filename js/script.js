0b80a103d1903244bc980bfa9eedf600e9fce322
Your Name <you@example.com>
2020-03-23 11:29:54 -0600

    javascript to initialize tools and plans

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ class Box
     {
         this.hoverMessage = "Click me for more detail";
     }
+    retHovMsg()
+    {
+        return this.hoverMessage;
+    }
 }
 
 class Templates extends Box
@@ -12,6 +16,11 @@ class Templates extends Box
     {
         super();
     }
+
+    retHovMsg ()
+    {
+        return super.retHovMsg();
+    }
 }
 class Activities extends Box
 {
@@ -19,22 +28,42 @@ class Activities extends Box
     {
         super();
     }
+    retHovMsg ()
+    {
+        return super.retHovMsg();
+    }
 }
 
 
+init_Templates = () =>
+{
+    var boxes = document.getElementsByClassName("plan");
+    var box_obj = new Templates();
+    console.log(boxes);
+    for (let i = 0; i < boxes.length; i++)
+    {
+        boxes[i].title = box_obj.retHovMsg();
+        boxes[i].parentElement.style.position = "relative";
+        boxes[i].parentElement.style.textAlign = "center";
+    }
+}
+init_Tools = () =>
+{
+    var boxes = document.getElementsByClassName("tool");
+    var box_obj = new Activities();
+    console.log(boxes);
+    for (let i = 0; i < boxes.length; i++)
+    {
+        boxes[i].title = box_obj.retHovMsg();
+        boxes[i].parentElement.style.position = "relative";
+        boxes[i].parentElement.style.textAlign = "center";
+    }
+}
 
-var boxes = document.getElementsByClassName("top-box")
-console.log(boxes);
-
-var box_obj = new Box();
-console.log(boxes[0]);
-boxes[0].title = box_obj.hoverMessage;
-boxes[0].parentElement.style.position = "relative";
-boxes[0].parentElement.style.textAlign = "center";
-var child = boxes[0].parentElement.childNodes;
-child[3].textContent = "Business Case";
-child[3].title = box_obj.hoverMessage;
-child[3].style.userSelect = "none";
+// var child = boxes[0].parentElement.childNodes;
+// child[3].textContent = "Business Case";
+// child[3].title = box_obj.retHovMsg();
+// child[3].style.userSelect = "none";
 
 // var template_obj = new Templates();
 // console.log(boxes[0]);
@@ -43,3 +72,6 @@ child[3].style.userSelect = "none";
 // var activity_obj = new Activities();
 // console.log(boxes[0]);
 // boxes[2].innerHTML = activity_obj.typeMessage;
+
+init_Templates();
+init_Tools();
