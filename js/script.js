281d81478e2ba5d7eac74fc5c3550da46a73f2da
Your Name <you@example.com>
2020-03-24 16:13:35 -0600

    pop up with links and descriptions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 class Box
 {
     constructor()
@@ -21,6 +22,49 @@ class Templates extends Box
     {
         return super.retHovMsg();
     }
+
+    popUp = (name) =>
+    {
+        var filename = name;
+        filename = filename.replace(/\s+/g, '-').toLowerCase();
+        var newName = window.open();
+        
+        var heading1 = newName.document.createElement("h1");
+        var heading2 = newName.document.createElement("h2");
+        var text = ' Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, dolorem. ';
+        heading1.innerHTML = name;
+        heading2.innerHTML = text;
+        newName.document.body.appendChild(heading1);
+        newName.document.body.appendChild(heading2);
+        
+        var unorder = newName.document.createElement("ul");
+        var unLabel = newName.document.createElement('h3');
+        var tempLink = newName.document.createElement('a');
+
+        unLabel.innerHTML = "Other Templates:";
+        newName.document.body.append(unLabel);
+        newName.document.body.appendChild(unorder);
+        
+        for (let i = 0; i < 5; i++)
+        {
+            var listItem = newName.document.createElement("li");
+            listItem.innerHTML = `Template ${i}`;
+            unorder.appendChild(listItem);
+        }
+
+        tempLink.innerHTML = name;
+        tempLink.href = `www.${filename}.html`;
+        newName.document.body.append(tempLink);
+
+        /*  
+            New Window
+            Description of the template
+            Download Link - provided by client / Subscription Box
+            A link to the page in the knowledge base
+            Editable by client admin.
+
+        */
+    }
 }
 class Activities extends Box
 {
@@ -32,8 +76,51 @@ class Activities extends Box
     {
         return super.retHovMsg();
     }
-}
 
+    popUp = (name) =>
+    {
+        var filename = name;
+        filename = filename.replace(/\s+/g, '-').toLowerCase();
+        var newName = window.open();
+        
+        var heading1 = newName.document.createElement("h1");
+        var heading2 = newName.document.createElement("h2");
+        var text = ' Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, dolorem. ';
+        heading1.innerHTML = name;
+        heading2.innerHTML = text;
+        newName.document.body.appendChild(heading1);
+        newName.document.body.appendChild(heading2);
+        
+        var unorder = newName.document.createElement("ul");
+        var unLabel = newName.document.createElement('h3');
+        var tempLink = newName.document.createElement('a');
+
+        unLabel.innerHTML = "Other Templates:";
+        newName.document.body.append(unLabel);
+        newName.document.body.appendChild(unorder);
+        
+        for (let i = 0; i < 5; i++)
+        {
+            var listItem = newName.document.createElement("li");
+            listItem.innerHTML = `Template ${i}`;
+            unorder.appendChild(listItem);
+        }
+
+        tempLink.innerHTML = name;
+        tempLink.href = `www.${filename}.html`;
+        newName.document.body.append(tempLink);
+        
+
+
+        /*
+            New Window
+            Description of the activity
+            List of templates related to the activity with links / Subscription Box
+            Link to page in knowledge base
+            All links editable
+        */
+    }
+}
 
 init_Templates = () =>
 {
@@ -42,6 +129,7 @@ init_Templates = () =>
     console.log(boxes);
     for (let i = 0; i < boxes.length; i++)
     {
+        /* CSS AND HTML INITIALIZATION */
         boxes[i].title = box_obj.retHovMsg();
         boxes[i].style.backgroundImage = "url('./resources/plan.png')";
         boxes[i].style.backgroundSize = "75px 75px";
@@ -51,12 +139,14 @@ init_Templates = () =>
         boxes[i].style.border = "none";
         boxes[i].parentElement.style.position = "relative";
         boxes[i].parentElement.style.textAlign = "center";
+
+        boxes[i].addEventListener("click", () => {box_obj.popUp(boxes[i].textContent);}, false);
     }
 }
 init_Tools = () =>
 {
     var boxes = document.getElementsByClassName("tool");
-    var box_obj = new Activities();
+    var box_obj = new Templates();
     console.log(boxes);
     for (let i = 0; i < boxes.length; i++)
     {
@@ -69,21 +159,22 @@ init_Tools = () =>
         boxes[i].style.border = "none";
         boxes[i].parentElement.style.position = "relative";
         boxes[i].parentElement.style.textAlign = "center";
+
+        boxes[i].addEventListener("click", () => {box_obj.popUp(boxes[i].textContent);}, false);
+    }
+}
+init_Activitives = () =>
+{
+    var boxes = document.getElementsByClassName("instruction");
+    var box_obj = new Activities();
+    console.log(boxes);
+    for (let i = 0; i < boxes.length; i++)
+    {
+        boxes[i].title = box_obj.retHovMsg();
+        boxes[i].addEventListener("click", () => {box_obj.popUp(boxes[i].textContent);}, false);
     }
 }
 
-// var child = boxes[0].parentElement.childNodes;
-// child[3].textContent = "Business Case";
-// child[3].title = box_obj.retHovMsg();
-// child[3].style.userSelect = "none";
-
-// var template_obj = new Templates();
-// console.log(boxes[0]);
-// boxes[1].innerHTML = template_obj.typeMessage;
-
-// var activity_obj = new Activities();
-// console.log(boxes[0]);
-// boxes[2].innerHTML = activity_obj.typeMessage;
-
 init_Templates();
 init_Tools();
+init_Activitives();
